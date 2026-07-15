e6b309adaae4ecc67a3f51ab7910bb1d3632673e
Adefokun Adeoluwa Israel <adefokunadeoluwaisrael@gmail.com>
2026-07-15 14:27:03 +0100

    update

---
+++ script.js
@@ -0,0 +1,18 @@
+const name = "ade";
+// console.log(name)
+document.getElementById("demo").innerHTML= "hello ade";
+
+const BigNum = 30
+const SmallNum = 4
+
+if(SmallNum > BigNum){
+    console.log("bigger")
+}else if(SmallNum === 4){
+    console.log("smaller")
+}else{
+    console.log("nothing")
+}
+
+// else(
+//     console.log("false")
+// )
