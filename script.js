60ccb7f458e3b97d6fea8ff7bfd40a247d5d56b3
Adefokun Adeoluwa Israel <adefokunadeoluwaisrael@gmail.com>
2026-07-20 13:57:59 +0100

    javascript into

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-const name = "ade";
-// console.log(name)
-document.getElementById("demo").innerHTML= "hello ade";
+// const name = "ade";
+// // console.log(name)
+// document.getElementById("demo").innerHTML= "hello ade";
 
-const BigNum = 30
-const SmallNum = 4
+// const BigNum = 30
+// const SmallNum = 4
 
-if(SmallNum > BigNum){
-    console.log("bigger")
-}else if(SmallNum === 4){
-    console.log("smaller")
-}else{
-    console.log("nothing")
-}
+// if(SmallNum > BigNum){
+//     console.log("bigger")
+// }else if(SmallNum === 4){
+//     console.log("smaller")
+// }else{
+//     console.log("nothing")
+// }
 
-// else(
-//     console.log("false")
-// )
+// // else(
+// //     console.log("false")
+// // )
+
+console.log("hello world")
+console.log("Hello!");
+console.log(100);
+console.log(true);
+console.log("My name is Ada");
+
+const age = 90;
+
+age = 100
+
+console.log(age);
