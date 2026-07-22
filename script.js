81303d343781c8f4022ca97cba05910c4d876455
Adefokun Adeoluwa Israel <adefokunadeoluwaisrael@gmail.com>
2026-07-22 14:51:30 +0100

    update

--- script.js
+++ script.js
@@ -17,14 +17,59 @@
 // //     console.log("false")
 // // )
 
-console.log("hello world")
-console.log("Hello!");
-console.log(100);
-console.log(true);
-console.log("My name is Ada");
+// console.log("hello world")
+// console.log("Hello!");
+// console.log(100);
+// console.log(true);
+// console.log("My name is Ada");
 
-const age = 90;
+// const age = 90;
 
-age = 100
+// age = 100
 
-console.log(age);
+// console.log(age);
+
+
+// const age = 50
+// var city = "lagos"
+// const name = "prada"
+// const num = 10
+
+// if (num % 3 === 0){
+//     console.log("Even Number")
+// }else{
+//     console.log("Odd number")
+// }
+
+// else if(age > 20){
+//     console.log("older")
+// }
+
+// let x = 8;
+// let y = 10
+
+// console.log(x != y)
+
+// if(!x){
+//     console.log("5 is great")
+// }
+
+let name = "";
+
+name === "ade" ? console.log("Ade is the name") : console.log("uhmmm")
+
+// switch("ade"){
+//     case "ade": 
+//     name = "ade"
+//     console.log("Ade is the Name")
+//     break
+
+//     case "kola":
+//     name = "kola"
+//     break
+
+//     case "bisi": 
+//     name ="bisi"
+
+// }
+
